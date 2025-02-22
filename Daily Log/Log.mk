36528ee018b098fb1ef83fce656cb1b7ed02ab6d
JordiHub80 <jorferrtorr@gmail.com>
2025-02-22 18:09:47 +0100

    Create Log.mk

---
+++ Daily Log/Log.mk
@@ -0,0 +1,6 @@
+# Programming Daily Log
+
+## 22-02-2025
+In this log I will write down everything any thoughts I have in the process of learning code especifcally for game developpement.
+Today, for example, I reopened Visual Studio for the first time in months, and I'm relearning how to take notes using GitHub to familiarize 
+myself with the process and hopefully build a daily habit. 
